d67cb885f399f5ae23aecd9c5a904b8ae0784383
Shawdooow <shawdooow@Shawns-Mac-mini.local>
2019-01-01 08:53:04 -0500

    Ip swap

--- Symcol.Server/Screens/HomeScreen.cs
+++ Symcol.Server/Screens/HomeScreen.cs
@@ -268,7 +268,7 @@ namespace Symcol.Server.Screens
                 } 
             });
 
-            games.Add(new GameItem("osu", "osu", "10.0.0.24:25590"));
+            games.Add(new GameItem("osu", "osu", "10.0.0.108:25590"));
         }
     }
 }
